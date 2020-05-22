2791e79df2bc89f277851b403a846af50f9da751
Peter Simacek <peter.simacek@yahoo.de>
2020-05-22 09:14:14 +0200

    created spring web app with eclipse tool suite

---
+++ src/main/java/peterSpringPractice/PracticeWebApp/PracticeWebAppApplication.java
@@ -0,0 +1,13 @@
+package peterSpringPractice.PracticeWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/peterSpringPractice/PracticeWebApp/PracticeWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package peterSpringPractice.PracticeWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
